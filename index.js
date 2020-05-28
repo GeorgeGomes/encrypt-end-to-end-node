91e4344d6a5b5e68ffd4545a92191204e97941c6
George <george@4move.me>
2020-05-28 20:34:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//https://coolaj86.com/articles/asymmetric-public--private-key-encryption-in-node-js/
+
+const crypto = require('crypto')
+const path = require('path')
+const fs = require('fs')
+
+function encrypt(toEncrypt, relativeOrAbsolutePathToPublicKey) {
+  const absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey)
+  const publicKey = fs.readFileSync(absolutePath, 'utf8')
+  const buffer = Buffer.from(toEncrypt, 'utf8')
+  const encrypted = crypto.publicEncrypt(publicKey, buffer)
+  return encrypted.toString('base64')
+}
+
+function decrypt(toDecrypt, relativeOrAbsolutePathtoPrivateKey) {
+  const absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey)
+  const privateKey = fs.readFileSync(absolutePath, 'utf8')
+  const buffer = Buffer.from(toDecrypt, 'base64')
+  const decrypted = crypto.privateDecrypt(
+    {
+      key: privateKey.toString(),
+      passphrase: '',
+    },
+    buffer,
+  )
+  return decrypted.toString('utf8')
+}
+
+const file = fs.readFileSync('./file_without_encrypt.txt', 'utf8')
+
+const enc = encrypt(file, 'publickey.pem')
+console.log('enc', enc)
+
+const dec = decrypt(enc, 'privatekey.pem')
+console.log('dec', dec)
